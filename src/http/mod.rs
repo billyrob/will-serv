edceae4abe1df3e046fb74374f5babc8502616f6
wrobinson <wrobinson@demonware.net>
2021-04-05 19:21:04 +0100

    Add tests to HTTP module + small fixes highlighted by tests

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use chrono::{DateTime, Utc};
 pub const BODY_DELIMINATER: &str = "\r\n\r\n";
+pub const VERSION_STR: &str = "0.0.1";
 
 pub fn process_request(input_buffer: &str, resource_map: & HashMap<String, String>) -> String {
     let mut header_map: HashMap<&str, &str> = HashMap::new();
@@ -9,15 +10,25 @@ pub fn process_request(input_buffer: &str, resource_map: & HashMap<String, Strin
     let mut path: &str = "";
 
     let v: Vec<&str> = input_buffer.splitn(2, BODY_DELIMINATER).collect();
+    // The request must be delimited by the BODY_DELIMINATER
+    if v.len() < 2 {
+        let now: DateTime<Utc> = Utc::now();
+        return format!("HTTP/1.1 400 Bad Request\nServer: will-serv/{}\nDate: {}{}",
+            VERSION_STR, now.to_rfc2822(), BODY_DELIMINATER);
+    }
     let headers = v[0];
     let _body = v[1];
     for line in headers.lines() {
         if line_num == 0 {
+            // Read the Request Line
+            // https://tools.ietf.org/html/rfc7230#section-3.1.1
             let pieces: Vec<&str> = line.splitn(3, ' ').collect();
             _method = pieces[0];
             path = pieces[1];
         }
         else {
+            // Read Headers
+            // https://tools.ietf.org/html/rfc7230#section-3.2
             let pieces: Vec<&str> = line.splitn(2, ':').collect();
             let header_key = pieces[0];
             let header_value = pieces[1].trim();
@@ -27,14 +38,61 @@ pub fn process_request(input_buffer: &str, resource_map: & HashMap<String, Strin
     }
     let now: DateTime<Utc> = Utc::now();
     if resource_map.contains_key(path) {
-        let html = resource_map.get(path).expect("ASDF");
+        let html = resource_map.get(path).expect("Failed to request HTML");
         return format!(
-            "HTTP/1.1 200 OK\nServer: will-serv/0.0.1\nDate: {}\nContent-Type: text/html\nContent-Length: {}{}{}",
-            now.to_rfc2822(), html.len(), BODY_DELIMINATER, html)
+            "HTTP/1.1 200 OK\nServer: will-serv/{}\nDate: {}\nContent-Type: text/html\nContent-Length: {}{}{}",
+            VERSION_STR, now.to_rfc2822(), html.len(), BODY_DELIMINATER, html);
     }
     else {
-        return format!("HTTP/1.1 404 Not Found\nServer: will-serv/0.0.1\nDate: {}{}",
-            now.to_rfc2822(), BODY_DELIMINATER);
+        return format!("HTTP/1.1 404 Not Found\nServer: will-serv/{}\nDate: {}{}",
+            VERSION_STR, now.to_rfc2822(), BODY_DELIMINATER);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_happy_path() {
+        let mut web_resources: HashMap<String, String> = HashMap::new();
+        web_resources.insert("/".to_string(), "<h1> TEST </h1>".to_string());
+        let get_request_buffer: &str = "GET / 1.1\n\r\n\r\n";
+        let response_html = process_request(get_request_buffer, & web_resources);
+
+        assert!(response_html.contains("HTTP/1.1 200 OK"));
+        assert!(response_html.contains("<h1> TEST </h1>"));
     }
 
+    #[test]
+    fn test_404() {
+        let mut web_resources: HashMap<String, String> = HashMap::new();
+        web_resources.insert("/".to_string(), "<h1> TEST </h1>".to_string());
+        let get_request_buffer: &str = "GET /NONEXISTENT 1.1\n\r\n\r\n";
+        let response_html = process_request(get_request_buffer, & web_resources);
+
+        assert!(response_html.contains("HTTP/1.1 404 Not Found"));
+    }
+
+    #[test]
+    fn test_many_headers() {
+        let mut web_resources: HashMap<String, String> = HashMap::new();
+        web_resources.insert("/".to_string(), "<h1> TEST </h1>".to_string());
+        let get_request_buffer: &str = "GET / 1.1
+        QWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\nQWER: ASDF\r\n\r\n";
+        let response_html = process_request(get_request_buffer, & web_resources);
+
+        assert!(response_html.contains("HTTP/1.1 200 OK"));
+        assert!(response_html.contains("<h1> TEST </h1>"));
+    }
+
+    #[test]
+    fn test_malformed() {
+        let mut web_resources: HashMap<String, String> = HashMap::new();
+        web_resources.insert("/".to_string(), "<h1> TEST </h1>".to_string());
+        let malformed_request: &str = "INVALIDMETHOD/NONEXISTENT1.1\n\r\r\n";
+        let response_html = process_request(malformed_request, & web_resources);
+
+        assert!(response_html.contains("HTTP/1.1 400 Bad Request"));
+    }
 }
